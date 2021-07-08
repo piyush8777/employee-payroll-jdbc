ee47b246d4424312f6a6f3ddad0d91a1d7aefbc9
Piyush Shaw <piyushshaw8777@gmail.com>
2021-07-08 12:46:24 +0530

    added : connect java program to database

---
+++ src/test/java/com/payrolljdbc/AppTest.java
@@ -0,0 +1,20 @@
+package com.payrolljdbc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
